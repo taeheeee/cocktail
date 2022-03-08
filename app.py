acc1fd625ed430516f69a4cd707ca7d96d557faa
kypark <pp2827768@naver.com>
2022-03-08 10:22:48 +0900

    complete paging

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-import requests
+import requests, math
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 app = Flask(__name__)
 
-
-@app.route('/')
-def home():
+@app.route('/<i>')
+def home(i):
     req = requests.get(f"https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
     result = req.json()
-    print(len(result['drinks']))
-    return render_template("index.html", result= result["drinks"])
+    return render_template("index.html", i = int(i), len = math.ceil(len(result['drinks'])//6), result= result["drinks"][6*(int(i)-1):6*int(i)])
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
