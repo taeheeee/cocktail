9a40aa8f5c58ee141845a37eadd7f48d2a9cf73a
kyoung sub <dltjq2323@gamil.com>
2022-03-08 19:06:44 +0900

    imgRd-jinja2 수정

--- templates/modules/list.py
+++ templates/modules/list.py
@@ -1,10 +1,14 @@
-from ..modules import * 
-from flask import Blueprint, render_template
+from flask import Flask, render_template
+import requests
 
+app = Flask(__name__)
 
-blueprint= Blueprint('list', __name__, template_folder="../html", url_prefix='/list')
+@app.route('/')
+def detail():
+    c = requests.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
+    response = c.json()
+    cocktails = response['drinks']
+    return render_template("list.html", cocktails=cocktails)
 
-
-@blueprint.route('/', methods=['GET'])
-def list_home():
-    return render_template('list.html' )
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
