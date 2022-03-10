6fb3cbce6a44b92701ffe4412a02fccb374cbd1d
diasm <diasm2@gmail.com>
2022-03-10 13:27:28 +0700

    정상 작동확인

--- app.py
+++ app.py
@@ -70,12 +70,13 @@ def home(i=1):
     elif is_login == "fail":
         user_data = None
 
-    
+    r = requests.get(f"http://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
+    result2 = r.json()
 
     req = requests.get(f"https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
     result = req.json()
     if i == "favicon.ico": i = 1
-    return render_template("index.html", user_data = user_data, len = math.ceil(len(result['drinks'])/6), result= result["drinks"][6*(int(i)-1):6*int(i)])
+    return render_template("index.html", result2 = result2, user_data = user_data, len = math.ceil(len(result['drinks'])/6), result= result["drinks"][6*(int(i)-1):6*int(i)])
 
 
 @app.route('/favorte')
@@ -249,6 +250,35 @@ def delete_user_favorite():
     return jsonify({'msg': '즐겨찾기에 제거되었습니다.'})
 
 
+
+def searchByName(keyword):
+    r = requests.get(f"http://www.thecocktaildb.com/api/json/v1/1/search.php?s={keyword}")
+
+    result = r.json()['drinks']
+    list1 =[] 
+
+    for x in result:
+        list1.append(x['strDrink'])
+
+    return list1 
+
+
+
+
+##칵테일 이름으로 검색 결과 
+## End point
+## /search/result
+@app.route('/search/result', methods=["POST"] )
+def search():
+    keyword_receive = request.form['keyword_give']
+    result= searchByName(keyword_receive)
+    print(result)
+    if result is not None:
+        print("nothin") 
+        return jsonify({"result" : result, "msg" : "success"})
+    # return render_template("search.html", result2=result2 )
+    else : 
+        return jsonify({ "msg" : "failed"}) 
 
 
 
